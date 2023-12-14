63985a90d97f92f591f7db75231769be45380c20
Miguel Velasco <mvelasco@sion.com>
2023-12-14 16:40:51 -0300

    Agregue el about y servicios

--- diguel/diguel/public/script/index.js
+++ diguel/diguel/public/script/index.js
@@ -43,3 +43,84 @@
 //   circleOne.style.top = `${y / 1.1}px`; // Ajusta la velocidad de seguimiento cambiando este valor
 // });
 
+const softwareDevelopment = document.getElementById("section1");
+const productDesign = document.getElementById("section2");
+const serverArchitecture = document.getElementById("section3");
+const webDevelopment = document.getElementById("section4");
+
+const box1 = document.getElementById("boxSection1");
+const box2 = document.getElementById("boxSection2");
+const box3 = document.getElementById("boxSection3");
+const box4 = document.getElementById("boxSection4");
+
+const textContentSection = document.getElementById("textContentSection");
+
+const texts = [
+  "We specialize in crafting bespoke software solutions that empower businesses by tackling their challenges head-on. Our team thrives on unraveling complexities, seeking innovative paths, and delivering tailored software that resolves your unique needs, fostering growth and efficiency.",
+  "Our team of visionary designers crafts captivating interfaces, translating your ideas into stunning products. We blend creativity with functionality, ensuring user-centric designs that elevate your product, enhancing its appeal and usability.",
+  "With seasoned experts, we navigate the intricate realm of server architecture, tackling communication challenges between businesses. From crafting complex server ecosystems to seamless API integrations, our proficiency ensures robust solutions and streamlined operations.",
+  "Collaborating seamlessly with designers, we transform your web presence, ensuring an SEO-optimized platform. We tailor your site to fit your business needs, driving growth, enhancing visibility, and delivering a compelling user experience.",
+];
+
+let lastBox = null;
+let lastSection = null;
+
+softwareDevelopment.addEventListener("mouseenter", () => {
+  if (lastBox != null && lastSection != null) {
+    lastBox.style.backgroundColor = "#6d6d6d";
+    changeBorder(lastSection);
+  }
+  softwareDevelopment.style.borderBottom = '1px solid #6d6d6d';
+  changeText(texts[0]);
+  changeBackground(box1);
+  lastBox = box1;
+  lastSection = softwareDevelopment;
+});
+
+productDesign.addEventListener("mouseenter", () => {
+  if (lastBox != null && lastSection != null) {
+    lastBox.style.backgroundColor = "#6d6d6d";
+    changeBorder(lastSection);
+  }
+  productDesign.style.borderBottom = '1px solid #6d6d6d';
+  changeText(texts[1]);
+  changeBackground(box2);
+  lastBox = box2;
+  lastSection = productDesign;
+});
+
+serverArchitecture.addEventListener("mouseenter", () => {
+  if (lastBox != null && lastSection != null) {
+    lastBox.style.backgroundColor = "#6d6d6d";
+    changeBorder(lastSection);
+  }
+  serverArchitecture.style.borderBottom = '1px solid #6d6d6d';
+  changeText(texts[2]);
+  changeBackground(box3);
+  lastBox = box3;
+  lastSection = serverArchitecture;
+});
+
+webDevelopment.addEventListener("mouseenter", () => {
+  if (lastBox != null && lastSection != null) {
+    lastBox.style.backgroundColor = "#6d6d6d";
+    changeBorder(lastSection);
+  }
+  webDevelopment.style.borderBottom = '1px solid #6d6d6d';
+  changeText(texts[3]);
+  changeBackground(box4);
+  lastBox = box4;
+  lastSection = webDevelopment;
+});
+
+function changeText(texto) {
+  textContentSection.textContent = texto;
+}
+
+function changeBorder(section) {
+  section.style.borderBottom = "1px solid transparent";
+}
+
+function changeBackground(box) {
+  box.style.backgroundColor = "#fff";
+}
