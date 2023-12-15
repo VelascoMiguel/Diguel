b3dba74bd265503a4ea1d60d809923f15b1c518a
Miguel Velasco <mvelasco@sion.com>
2023-12-15 17:01:40 -0300

    Nuevos cambios

--- diguel/diguel/src/index.js
+++ diguel/diguel/src/index.js
@@ -1,37 +1,6 @@
-// const grid = document.querySelector(".grid");
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const cuadradoSize = 20; // Tamaño de cada cuadrado en píxeles
-//   const gridWidth = grid.clientWidth;
-//   const gridHeight = grid.clientHeight;
-
-//   const cuadradosHorizontales = Math.floor(gridWidth / cuadradoSize);
-//   const cuadradosVerticales = Math.floor(gridHeight / cuadradoSize);
-//   const totalCuadrados = cuadradosHorizontales * cuadradosVerticales;
-
-//   for (let index = 0; index < totalCuadrados; index++) {
-//     const newBox = document.createElement("div");
-//     newBox.classList.add("cuadrado");
-//     grid.appendChild(newBox);
-//   }
-
-//   const allBoxes = document.querySelectorAll(".cuadrado");
-
-//   allBoxes.forEach((box) => {
-//     box.addEventListener("mouseenter", () => {
-//       box.style.backgroundColor = "white";
-//     });
-
-//     box.addEventListener("mouseleave", () => {
-//       box.style.backgroundColor = "transparent"; // Vuelve al color original (puede ser el color inicial definido en CSS)
-//     });
-//   });
-// });
-
 const scrollCircle = document.querySelector(".scrollCircle");
 const circleOne = document.querySelector(".circleOne");
 const circleTwo = document.querySelector(".circleTwo");
-
 
 scrollCircle.addEventListener("mousemove", (e) => {
   const x = e.clientX - scrollCircle.getBoundingClientRect().left;
@@ -45,7 +14,7 @@ scrollCircle.addEventListener("mousemove", (e) => {
 });
 
 scrollCircle.addEventListener("mouseleave", () => {
-    // Pendiente
+  // Pendiente
 });
 
 const softwareDevelopment = document.getElementById("section1");
