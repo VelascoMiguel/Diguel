a31919f449cf792098946504b320ccf7b5def874
Miguel Velasco <mvelasco@sion.com>
2024-02-17 17:14:35 -0300

    Agregue PHP Mailer

---
+++ diguel/src/contact/mail.php
@@ -0,0 +1,39 @@
+<?php
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;  
+
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $presupuesto = $_POST['presupuesto'];
+    $nombreCompañia = $_POST['nombreCompañia'];
+    $mensaje = $_POST['message'];
+
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'velascomiguell610@gmail.com';
+    $mail->Password = 'Diealeroanmi2003!';
+    $mail->Port = 465;
+    $mail->SMTPSecure = 'ssl';
+    $mail->isHTML(true);
+    $mail->setFrom($email, $nombre);
+    $mail->addAddress('velascomiguell610@gmail.com');
+    $mail->Subject = ("$email ($asunto) ($nombreCompañia)");
+    $mail->Body = "Nombre: $nombre<br>";
+    $mail->Body .= "Email: $email<br>";
+    $mail->Body .= "Presupuesto: $presupuesto<br>";
+    $mail->Body .= "Nombre de la Compañía: $nombreCompañia<br>";
+    $mail->Body .= "Mensaje: $mensaje";
+    $mail->send();
+
+}
+
